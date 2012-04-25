6d07ff15fb5b8809180d380bd15a5b2f34c58847
Nils Olsson <nilso@enosis.net>
2012-04-25 11:14:40 -0700

    Worked on SJmount_Init

--- src/SJmount/SJmount_Init.lua
+++ src/SJmount/SJmount_Init.lua
@@ -1,6 +1,5 @@
 
-SJmount = Frame
-
+--- OnLoad
 function SJmount_OnLoad(frame)
 	-- Register Events
 	frame:RegisterEvent("ADDON_LOADED")
@@ -8,11 +7,24 @@ function SJmount_OnLoad(frame)
 	frame:RegisterEvent("LEARNED_SPELL_IN_TAB")
 	frame:RegisterEvent("COMPANION_LEARNED")
 	frame:RegisterEvent("UNIT_AURA")
+	frame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+	frame:RegisterEvent("UPDATE_WORLD_STATES")
+	-- Slash Commands
+	SlashCmdList["SJmount"] = SJmount_SCommand
+	SLASH_SJmount1 = "/sjmount"
+	SLASH_SJmount2 = "/sjm"
+	SLASH_SJmount3 = "/mount"
 end
 
+--- OnEvent
 function SJmount_OnEvent(frame, event, ...)
-	if event == "ADDON_LOADED"then
-	elseif event == "PLAYER_ENTERING_WORLD" then
+	if (event == "ADDON_LOADED") then
+		if (arg1 == "SJmount") then
+		end
+	elseif (event == "PLAYER_ENTERING_WORLD") then
+	end
+
+	if (event == "LEARNED_SPELL_IN_TAB") or (event == "COMPANION_LEARNED") then
 	end
 end
 
